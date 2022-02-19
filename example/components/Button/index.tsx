b26724959630b0b3c17cccc100be7a7ccb7986fa
Vlad Afonin <me@myt.su>
2022-02-20 01:54:57 +0300

    docs: example app

---
+++ example/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import { ComponentProps } from 'react'
+import cnj from 'cnj'
+
+import { $button } from './style.css'
+
+export default function Button({ className, ...props }: ComponentProps<'button'>) {
+	return <button className={cnj($button, className)} {...props} />
+}
--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,25 @@
+import { useReducer } from 'react'
 import { render } from 'react-dom'
-import Test from './test'
 
-import './styles.css'
+import Logo from './webpack_logo.svg'
+import Button from 'components/Button'
+
+import { $row, $logo } from './styles.css'
 
 function App() {
+	let [value, increment] = useReducer((acc) => acc + 1, 0)
+
 	return (
-		<div>
-			<Test />
-		</div>
+		<>
+			<h1 className={$row}>
+				<Logo className={$logo} />
+				Webpack Shared app example
+			</h1>
+			<div className={$row}>
+				<Button onClick={increment}>Add</Button>
+				Value: {value}
+			</div>
+		</>
 	)
 }
 
